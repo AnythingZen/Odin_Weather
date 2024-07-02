627fcee068414fc4d8eba92d41981952121ff5cb
anythingzen <chengzhanxd@gmail.com>
2024-07-03 00:05:39 +0800

    decouple displayWeatherData and fix callback updateTemperature bug. previously not responding on next input.

--- main.js
+++ main.js
@@ -2,8 +2,6 @@ const weatherForm = document.querySelector('.weatherForm');
 const placeInput = document.getElementById('search');
 
 const card = document.querySelector('.card');
-const body = document.querySelector('body');
-
 const API_KEY = '82af182be9c247a2bf7102756240107&q';
 
 weatherForm.addEventListener('submit', async (event) => {
@@ -14,8 +12,7 @@ weatherForm.addEventListener('submit', async (event) => {
     if (country) {
         try {
             const weatherData = await getWeatherData(country);
-            const updateTemperatureFunction = getTemperature(weatherData.current.temp_c, weatherData.current.temp_f);
-            displayWeatherData(weatherData, updateTemperatureFunction);
+            displayWeatherData(weatherData, updateTemperature);
         }
         catch(error) {
             console.log(error);
@@ -29,9 +26,9 @@ weatherForm.addEventListener('submit', async (event) => {
 })
 
 async function getWeatherData(country) {
-    const apiUrl = `http://api.weatherapi.com/v1/current.json?key=${API_KEY}=${country}`;
+    const apiUrl = `https://api.weatherapi.com/v1/current.json?key=${API_KEY}=${country}`;
 
-    const response = await fetch(apiUrl);
+    const response = await fetch(apiUrl, {mode: 'cors'});
 
     if (!response.ok) {
         throw new Error(`Unable to fetch data for [${country.toUpperCase()}]`);
@@ -40,76 +37,38 @@ async function getWeatherData(country) {
     return await response.json();
 }
 
-function displayWeatherData(weatherData, updateTemperatureFunction) {
+function displayWeatherData(weatherData, updateTemperature) {
     card.textContent = '';
     card.style.display = 'flex';
 
-    const location = document.createElement('h1');
-    const temperatureBlock = document.createElement('div');
-    const temperature = document.createElement('p');
-    const toggleSwitch = document.querySelector('.switch')
-    const currentWeather = document.createElement('p');
-    const weatherEmote = document.createElement('img');
+    const elements = createWeatherCard();
+    updateWeatherCard(weatherData, elements, updateTemperature);
+}
 
-    location.className = 'location';
-    temperatureBlock.className = 'temperatureBlock';
-    temperature.className = 'temperature';
-    currentWeather.className = 'currentWeather';
-    weatherEmote.className = 'weatherEmote';
+function updateWeatherCard(weatherData, elements, updateTemperature) {
+    const { location, temperature, currentWeather, weatherEmote } = elements;
 
-    temperatureBlock.appendChild(temperature)
-    temperatureBlock.appendChild(toggleSwitch);
-    card.appendChild(location);
-    card.appendChild(temperatureBlock);
-    card.appendChild(currentWeather);
-    card.appendChild(weatherEmote);
+    const { location: { name }, current: { condition: { icon, text }, temp_c, temp_f } } = weatherData;
 
-    const {location: {name},
-           current: {condition: { icon, text },
-           temp_c, temp_f,}} = weatherData;
+    let isCelsius = true;
+    let currentTemp = updateTemperature(isCelsius, temp_c, temp_f); // default is celcius
+    temperature.innerText = currentTemp;
 
-    let currentTemp = updateTemperatureFunction();
-    temperature.innerText = currentTemp; // default is celcius
-    const toggle = document.getElementById('toggle');
+    const toggleSwitch = document.getElementById('toggle');
 
-    toggle.addEventListener('click', () => {
-        currentTemp = updateTemperatureFunction();
+    toggleSwitch.addEventListener('click', () => {
+        isCelsius = !isCelsius;
+        currentTemp = updateTemperature(isCelsius, temp_c, temp_f);
         temperature.innerText = currentTemp;
     });
-    
+
     location.innerText = name;
     currentWeather.innerText = `It is currently ${text}`;
     weatherEmote.src = icon;
 }
 
-function displayError(error) {
-    const errorMessage = document.createElement('p');
-    errorMessage.className = 'errorMessage';
-
-    errorMessage.innerText = error;
-    errorMessage.style.fontWeight = 'bold'
-
-    card.textContent = '';
-    card.style.display = 'flex';
-    card.appendChild(errorMessage);
-}
-
-function getTemperature(celcius, fahrenheit) {
-    let isCelcius = true; // default = celcius
-
-    const toggle = createSwitchElement();
-    body.appendChild(toggle);
-    const toggleSwitch = document.querySelector('#toggle');
-
-    const updateTemperature = function() {
-        return isCelcius ? `${celcius} °C` : `${fahrenheit} ° F`;
-      };
-
-    toggleSwitch.addEventListener('click', () => {
-        isCelcius = !isCelcius;
-    })
-    
-    return updateTemperature;
+function updateTemperature(isCelcius, celcius, fahrenheit) {
+    return isCelcius ? `${celcius} °C` : `${fahrenheit} ° F`;
 }
 
 function createSwitchElement() {
@@ -128,3 +87,46 @@ function createSwitchElement() {
 
     return switchLabel;
 }
+
+function createWeatherCard() {
+    const location = document.createElement('h1');
+    location.className = 'location';
+
+    const temperatureBlock = document.createElement('div');
+    temperatureBlock.className = 'temperatureBlock';
+
+    const temperature = document.createElement('p');
+    temperature.className = 'temperature';
+
+    const toggleSwitch = createSwitchElement();
+
+    const currentWeather = document.createElement('p');
+    currentWeather.className = 'currentWeather';
+
+    const weatherEmote = document.createElement('img');
+    weatherEmote.className = 'weatherEmote';
+
+    temperatureBlock.appendChild(temperature);
+    temperatureBlock.appendChild(toggleSwitch);
+    card.appendChild(location);
+    card.appendChild(temperatureBlock);
+    card.appendChild(currentWeather);
+    card.appendChild(weatherEmote);
+
+    return { location, temperature, toggleSwitch, currentWeather, weatherEmote };
+}
+
+function displayError(error) {
+    const errorMessage = document.createElement('p');
+    errorMessage.className = 'errorMessage';
+
+    errorMessage.innerText = error;
+    errorMessage.style.fontWeight = 'bold'
+
+    card.textContent = '';
+    card.style.display = 'flex';
+    card.appendChild(errorMessage);
+}
+
+// submit -> get data -> displayData -> createWeatherCard -> updateWeatherCard
+// -> attach listener to toggle temp -> callback to updateTemperature 
